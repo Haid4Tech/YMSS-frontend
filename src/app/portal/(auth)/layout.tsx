dc71bd157155cf64ed64cc5d9472268fcd1ef12c
iamikay <jdon144@gmail.com>
2025-10-31 12:09:40 +0100

    fix: added auth image

--- src/app/portal/(auth)/layout.tsx
+++ src/app/portal/(auth)/layout.tsx
@@ -7,7 +7,7 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
       {/* Left side - Background image */}
       <div className="hidden lg:flex lg:w-1/2 relative">
         <Image
-          src="https://scontent-los2-1.cdninstagram.com/v/t39.30808-6/400000903_17892550097924893_4347508550007350066_n.jpg?stp=dst-jpg_e35_p1080x1080_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0uaW1hZ2VfdXJsZ2VuLjE0NDB4MTgwMC5zZHIuZjMwODA4LmRlZmF1bHRfaW1hZ2UuYzIifQ&_nc_ht=scontent-los2-1.cdninstagram.com&_nc_cat=101&_nc_oc=Q6cZ2QHGZCPwRtxB8ERSpxIHckS4gErOiXCjffdP7imT50A3MPuNAi4yoWtUSWAsLfqV5fZg_ujiy-79-qNtmYgGUv-7&_nc_ohc=10zkV6q75zgQ7kNvwF6Ykxo&_nc_gid=PsXvcagykfZsarfqqEn2cw&edm=ALQROFkAAAAA&ccb=7-5&ig_cache_key=MzIzMTcyOTkzMzg0MjgzOTYyMw%3D%3D.3-ccb7-5&oh=00_AfZlELL3BOR3ks6Z1W3JiC7XOkawGhCkvhXiKq-yhQz1bA&oe=68DFE230&_nc_sid=fc8dfb"
+          src="/images/ymss_cover.jpg"
           alt="School Campus"
           fill
           className="object-cover"
